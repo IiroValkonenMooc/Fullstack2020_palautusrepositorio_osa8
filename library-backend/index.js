40d6bf52d74d13bc4be76b2a96ea4b5acc3c1e1a
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-09-04 03:23:25 +0300

    tehtävät 8.1 - 8.7 tehty

--- library-backend/index.js
+++ library-backend/index.js
@@ -75,7 +75,7 @@ let books = [
     genres: ['classic', 'crime']
   },
   {
-    title: 'The Demon ',
+    title: 'The Demon',
     published: 1872,
     author: 'Fyodor Dostoevsky',
     id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
@@ -84,14 +84,112 @@ let books = [
 ]
 
 const typeDefs = gql`
-  
+  type Book { 
+    title: String!,
+    published: Int!,
+    author: String!,
+    id: ID!,
+    genres: [String]
+  }
+
+  type Author { 
+    name: String!,
+    id: ID!,
+    born: Int
+    bookCount: Int
+  }
 
   type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [ Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!,
+      author: String!,
+      published: Int!,
+      genres: [String!]!
+    ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () =>  books.length,
+    authorCount: () =>  authors.length,
+    allBooks: (root, args) => {
+      if (!args.author&&!args.genre) {
+        return books
+      }
+
+      let allBooksBy = []
+      for (let i = 0; i < books.length; i++) {
+        if(books[i].author===args.author||books[i].genres.includes(args.genre)){
+          allBooksBy.push(books[i])
+        }
+      }
+      return allBooksBy
+    },
+    allAuthors: () => {
+      let authorsWithBookCount = []
+      for (let i = 0; i < authors.length; i++) {
+        let bookCount = 0
+        for (let j = 0; j < books.length; j++) {
+          if(authors[i].name===books[j].author){
+            bookCount++
+          }
+        }
+        authorsWithBookCount.push(
+          {
+            ...authors[i],
+            bookCount
+          }
+        )
+      }
+      console.log('authorsWithBookCount :>> ', authorsWithBookCount);
+      return authorsWithBookCount
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      books = [...books, { ...args } ]
+
+      let authorNames = []
+      for (let i = 0; i < authors.length; i++) {
+        authorNames.push(authors[i].name)
+      }
+      if(!authorNames.includes(args.author)){
+        authors=[
+          ...authors,
+          { name: args.author }
+        ]
+      }
+
+      console.log('authors :>> ', authors)
+      console.log('books :>> ', books)
+
+      return args
+    },
+    editAuthor: (root, args) => {
+      let foundAuthor = authors.find(author => author.name===args.name)
+
+      if(!foundAuthor){
+        return null
+      }else{
+        foundAuthor.born=args.setBornTo
+      }
+
+      console.log('authors :>> ', authors)
+      return foundAuthor
+    }
   }
 }
 
